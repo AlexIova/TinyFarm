8c31323529afd73f45a6ff6d9899c3207a2abc18
AlexIova <iovaalexandru37@gmail.com>
2022-05-05 13:06:01 +0200

    Added argument control

--- farm.c
+++ farm.c
@@ -2,7 +2,38 @@
 
 
 int main(int argc, char *argv[])
-{  
-	
+{
+  /* controllo argomenti con getopt(3)*/
+  int nthread = 4;
+  int qlen = 8;
+  int delay = 0;
+
+  int opt; 
+  
+  while ((opt = getopt(argc, argv, "n:q:t:")) != -1) {
+    switch(opt){
+      case 'n':
+        if (atoi(optarg) <= 0)
+          chiudi("Il numero di thread non può essere negativo, terminazione...");
+        else
+          nthread = atoi(optarg);
+        break;
+      case 'q':
+        if (atoi(optarg) <= 0)
+          chiudi("La lunghezza del buffer non può essere negativa, terminazione...");
+        else
+          qlen = atoi(optarg);
+        break;
+      case 't':
+        if (atoi(optarg) <= 0)
+          chiudi("Il ritardo non può essere negativo, terminazione...");
+        else
+          delay = atoi(optarg);
+        break;
+      default:
+        chiudi("Uso: \n  farm [flags]\n\nFLAGS:\n -n: numero thread\n -q: lunghezza buffer prodcons\n -t: delay tra richieste\n");
+    }
+  }
+	fprintf(stderr,"Valore argomenti:\nnthread = %d\nqlen = %d\ndelay = %d\n\n", nthread, qlen, delay);
 	return 0;
 }
