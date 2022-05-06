3e9669e2f05b3a51930bd9cca7293a1e05ed686f
AlexIova <iovaalexandru37@gmail.com>
2022-05-06 10:02:32 +0200

    Socket connection server working

--- collector.py
+++ collector.py
@@ -1,6 +1,27 @@
 #! /usr/bin/env python3
 
-# NOTA: Per identazione viene usato il tab
-import sys, struct, socket, threading, socketserver
+# NOTA: Per identazione vengono usati 4 spazi
+import sys, struct, socket, threading
 
-print("Hello world")
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+def main():
+    # Creazione server socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # SOCK_STREAM == TCP
+        try:  
+            s.bind((HOST, PORT))
+            s.listen()
+            while True:
+                print("In attesa di un client...")
+                conn, addr = s.accept()
+                print(f"Apparso un client\nconn: {conn}\taddr: {addr}")
+                # t = threading.Thread(target=gestisci_connessione, args=(conn,addr))
+                # t = ClientThread(conn,addr)
+                # t.start()
+        except KeyboardInterrupt:
+            pass
+    print('Va bene smetto...')
+    s.shutdown(socket.SHUT_RDWR)
+
+main()
