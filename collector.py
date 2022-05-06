dd1e54e42bcc28b30590fb46f24ba128c9a36ef3
AlexIova <iovaalexandru37@gmail.com>
2022-05-06 10:12:27 +0200

    Added client threads

--- collector.py
+++ collector.py
@@ -6,6 +6,23 @@ import sys, struct, socket, threading
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+
+def placeholder(n):
+    print(f"mi hanno passato il numero {n}")
+
+
+# Codice thread per gestione un client, sottoclasse di threading.Thread
+class ClientThread(threading.Thread):
+    def __init__(self, conn, addr):
+        threading.Thread.__init__(self)     # Inizializzazione superclasse
+        self.conn = conn
+        self.addr = addr
+    def run(self):
+        print(f"Sono {self.ident}, gestisco {self.addr}")
+        placeholder(50)
+        
+
+
 def main():
     # Creazione server socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # SOCK_STREAM == TCP
@@ -16,9 +33,9 @@ def main():
                 print("In attesa di un client...")
                 conn, addr = s.accept()
                 print(f"Apparso un client\nconn: {conn}\taddr: {addr}")
-                # t = threading.Thread(target=gestisci_connessione, args=(conn,addr))
-                # t = ClientThread(conn,addr)
-                # t.start()
+                t = threading.Thread(target=placeholder, args=(conn,addr))
+                t = ClientThread(conn,addr)
+                t.start()
         except KeyboardInterrupt:
             pass
     print('Va bene smetto...')
