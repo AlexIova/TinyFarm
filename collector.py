202905a73c04c274cf3de50abf57dd2d41076f5b
AlexIova <iovaalexandru37@gmail.com>
2022-05-07 15:39:45 +0200

    Refactored for using socketserver module

--- collector.py
+++ collector.py
@@ -1,79 +1,41 @@
 #! /usr/bin/env python3
-
-# NOTA: Per identazione vengono usati 4 spazi
+# server che fornisce l'elenco dei primi in un dato intervallo 
+# gestisce più clienti contemporaneamente usando i thread
 import sys, struct, socket, threading
+# modulo che contiene già un server bastao sui socket
+import socketserver
 
+# host e porta di default
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+ 
+class stampaSomme(socketserver.StreamRequestHandler):
+  def handle(self):
+    data = self.rfile.read(4)
+    assert len(data) == 4   # Deve essere un intero
+    byteNome = struct.unpack("!i", data)[0]     #"!i" network byte int
+    print(f"byteNome: {byteNome}")
+    if byteNome == -1:
+        print("ARRIVATO -1")
+        return
+    data = self.rfile.read(byteNome+8)      # stringa + long
+    nomeFile = data[:byteNome].decode("utf-8")
+    somma = struct.unpack("!q", data[-8:])[0]
+    print(f"{somma}\t{nomeFile}")
+    self.wfile.write(struct.pack("!i",1))   # Invio segnale di ACK
+
+
+# classe che specifica che voglio un TCP server gestito con i thread 
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    daemon_threads = True
+    allow_reuse_address = True
+    
+    
+def main(host=HOST,port=PORT):
+  with ThreadedTCPServer((host, port), stampaSomme) as server:
+    print('In attesa dei client...')
+    server.serve_forever()
 
 
 
-# Analogo readn
-def recv_all(conn,n):
-  chunks = b''
-  bytes_recd = 0
-  while bytes_recd < n:
-    chunk = conn.recv(min(n - bytes_recd, 1024))
-    if len(chunk) == 0:
-      raise RuntimeError("socket connection broken")
-    chunks += chunk
-    bytes_recd = bytes_recd + len(chunk)
-  return chunks
-
-
-
-def stampaSomme(conn, addr, fine):
-    with conn:
-        data = recv_all(conn, 4)
-        assert len(data) == 4   # Deve essere un intero
-        byteNome = struct.unpack("!i", data)[0]     #"!i" network byte int 
-        print(f"byteNome: {byteNome}")
-        if byteNome == -1:
-            fine.set()
-            return
-        data = recv_all(conn, byteNome + 8)     # stringa + long
-        nomeFile = data[:byteNome].decode("utf-8")
-        somma = struct.unpack("!q", data[-8:])[0]
-        print(f"{somma}\t{nomeFile}")
-
-
-
-# Codice thread per gestione un client, sottoclasse di threading.Thread
-class ClientThread(threading.Thread):
-    def __init__(self, conn, addr, fine):
-        threading.Thread.__init__(self)     # Inizializzazione superclasse
-        self.conn = conn
-        self.addr = addr
-        self.fine = fine
-    def run(self):
-        # print(f"Sono {self.ident}, gestisco {self.addr}")
-        stampaSomme(self.conn, self.addr, self.fine)
-        
-
-
-def main():
-    # Creazione server socket
-    fine = threading.Event()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # SOCK_STREAM == TCP
-        try:  
-            s.bind((HOST, PORT))
-            s.listen()
-            while True:
-                # print("In attesa di un client...")
-                conn, addr = s.accept()
-                t = threading.Thread(target=stampaSomme, args=(conn,addr,fine))
-                t = ClientThread(conn,addr,fine)
-                t.start()
-                if fine.isSet():
-                    break 
-        except KeyboardInterrupt:
-            pass
-    print('Va bene smetto...')
-    s.shutdown(socket.SHUT_RDWR)
-    sock.close() # Dealloca la memoria per la socket
-
-
-# collector.py non prende argomenti
 main()
-
-
